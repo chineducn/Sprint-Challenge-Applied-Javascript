40e946e27106c7081a9534de9a95cbf9e1882420
unknown <neduvinci@gmail.com>
2019-09-06 14:05:59 +0100

    continue on cards index.js by creating component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,6 +18,9 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
+
+//Actual code
+
 // axios.get('https://lambda-times-backend.herokuapp.com/articles')
 //     .then(articleYes => {
 //     debugger
@@ -26,7 +29,44 @@
 //     debugger
 // })
 
-const cardContainer = document.createElement('div');
-const headlineContainer = document.createElement('div');
-const authorCreator = document.createElement('div');
-const imageContainer = 
+//Variable declaration
+const cardContainer = document.querySelector('.cards-container');
+const cardItem = document.createElement('div');
+const headlineItem = document.createElement('div');
+const authorContainer = document.createElement('div');
+const imageContainer = document.createElement('div');
+const imageItem = document.createElement('img');
+const authorItem = document.createElement('span');
+
+//Attributing
+cardItem.classList.add('card');
+headlineItem.classList.add('headline');
+authorContainer.classList.add('author');
+imageContainer.classList.add('img-container');
+imageItem.src = '..';
+
+//Value giving
+headlineItem.textContent = 'headline placeholder';
+
+
+//Appending
+imageContainer.appendChild(imageItem);
+authorContainer.appendChild(imageContainer);
+authorContainer.appendChild(authorItem);
+cardItem.appendChild(headlineItem);
+cardItem.appendChild(authorContainer);
+cardContainer.appendChild(cardItem);
+
+
+
+
+
+
+//Test code
+console.log(cardItem);
+// console.log(headlineItem);
+// console.log(authorContainer);
+// console.log(imageContainer);
+// console.log(imageItem);
+// console.log(authorItem);
+// console.log(cardContainer)
