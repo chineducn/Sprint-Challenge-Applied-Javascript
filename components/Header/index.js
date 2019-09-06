91e56e1db45beebc28a37dd5b8e79af02a7fbf2c
unknown <neduvinci@gmail.com>
2019-09-06 09:43:43 +0100

    completed and displayed header componet

--- components/Header/index.js
+++ components/Header/index.js
@@ -14,4 +14,24 @@ function Header(date, title, temperature) {
     const span1Creator = document.createElement('span');
     const headingCreator = document.createElement('h1');
     const span2Creator = document.createElement('span');
+
+    divCreator.classList.add('header');
+    span1Creator.classList.add('date');
+    span2Creator.classList.add('temp');
+
+    span1Creator.textContent = date;
+    headingCreator.textContent = title;
+    span2Creator.textContent = temperature;
+
+    divCreator.appendChild(span1Creator);
+    divCreator.appendChild(headingCreator);
+    divCreator.appendChild(span2Creator);
+
+    return divCreator;
 }
+
+const headerComponent = Header('SMARCH 28, 2019', 'Lambda Times', '98°');
+
+const headerContainer = document.querySelector('.header-container');
+
+headerContainer.appendChild(headerComponent);
